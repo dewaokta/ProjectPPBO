e11eea2afb51395cb9156999d4347e0532df35bb
Taufiq-Al91 <85861000+Taufiq-Al91@users.noreply.github.com>
2021-07-20 22:56:39 +0700

    Add files via upload

---
+++ DataBase/puskesmas2 (2).sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Jul 2021 pada 07.38
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `puskesmas2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `obat`
+--
+
+CREATE TABLE `obat` (
+  `kode_obat` varchar(100) NOT NULL,
+  `nama_obat` varchar(100) DEFAULT NULL,
+  `harga_obat` varchar(100) DEFAULT NULL,
+  `stok` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`kode_obat`, `nama_obat`, `harga_obat`, `stok`) VALUES
+('KO-001', 'Paracetamol', '40000', 16),
+('KO-002', 'Amoxillin', '50000', 21);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id_pasien` varchar(100) NOT NULL,
+  `nama_lengkap_pasien` varchar(100) DEFAULT NULL,
+  `jenis_kelamin` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`id_pasien`, `nama_lengkap_pasien`, `jenis_kelamin`) VALUES
+('IP-001', 'Lim Raize', 'Laki-laki'),
+('IP-002', 'Lisa', 'Perempuan'),
+('IP-003', 'Mei Aprilia', 'Perempuan'),
+('IP-004', 'Luna', 'Perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id_pegawai` varchar(100) NOT NULL,
+  `nama_pegawai` varchar(100) DEFAULT NULL,
+  `jenis_kelamin` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pegawai`
+--
+
+INSERT INTO `pegawai` (`id_pegawai`, `nama_pegawai`, `jenis_kelamin`) VALUES
+('B-001', 'Emilia Raize', 'Perempuan'),
+('B-002', 'Dina Andini', 'Perempuan'),
+('B-003', 'Vergil Sparda', 'Laki-Laki'),
+('B-004', 'Iona Diva', 'Perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id_supplier` varchar(100) NOT NULL,
+  `kode_obat` varchar(100) DEFAULT NULL,
+  `nama_supplier` varchar(100) DEFAULT NULL,
+  `alamat_supplier` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `supplier`
+--
+
+INSERT INTO `supplier` (`id_supplier`, `kode_obat`, `nama_supplier`, `alamat_supplier`) VALUES
+('IS-001', 'KO-001', 'PT. Kimia Farma', 'Pekanbaru'),
+('IS-002', 'KO-002', 'PT. Kalbe Farma', 'Pekanbaru');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(100) NOT NULL,
+  `id_pegawai` varchar(100) DEFAULT NULL,
+  `id_pasien` varchar(100) DEFAULT NULL,
+  `kode_obat` varchar(100) DEFAULT NULL,
+  `jumlah_obat` int(10) DEFAULT NULL,
+  `total_pembayaran` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_pegawai`, `id_pasien`, `kode_obat`, `jumlah_obat`, `total_pembayaran`) VALUES
+('IT-001', 'B-001', 'IP-002', 'KO-001', 2, '80000');
+
+--
+-- Trigger `transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `Kurang_Stok` AFTER INSERT ON `transaksi` FOR EACH ROW BEGIN
+  UPDATE obat SET stok=stok-NEW.jumlah_obat WHERE kode_obat = NEW.kode_obat;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`kode_obat`);
+
+--
+-- Indeks untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
+-- Indeks untuk tabel `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id_pegawai`);
+
+--
+-- Indeks untuk tabel `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id_supplier`),
+  ADD KEY `kode_obat` (`kode_obat`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_pegawai` (`id_pegawai`),
+  ADD KEY `id_pasien` (`id_pasien`),
+  ADD KEY `kode_obat` (`kode_obat`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `supplier`
+--
+ALTER TABLE `supplier`
+  ADD CONSTRAINT `supplier_ibfk_1` FOREIGN KEY (`kode_obat`) REFERENCES `obat` (`kode_obat`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`),
+  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`kode_obat`) REFERENCES `obat` (`kode_obat`),
+  ADD CONSTRAINT `transaksi_ibfk_4` FOREIGN KEY (`kode_obat`) REFERENCES `obat` (`kode_obat`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
